6d33be46185fbf4b52b7da9b06d1859ecf9e45d2
Sebastian <sebas33696@gmail.com>
2025-02-08 15:31:02 +0100

    arreglado error de type

--- src/app/planets/page.tsx
+++ src/app/planets/page.tsx
@@ -2,7 +2,7 @@ import { prisma } from "@/lib/db"
 
 
 export default async function PlanetsPage() {
-    const planets = await prisma.Planets.findMany()
+    const planets = await prisma.planets.findMany()
     
 
     return (
